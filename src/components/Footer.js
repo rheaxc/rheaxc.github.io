3423c498bf97df55df6f4b3d2b1bab9ecda1dbfe
rheaxc <rheaxc@umich.edu>
2022-12-31 13:09:04 -0800

    footer: done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -25,16 +25,16 @@ class Footer extends React.Component {
             <div class="hover-overlay"></div>
               {(this.props.page === 'homepage') ? (
                 <div class="hover-3-content px-5 py-4">
-                  <h3 class="hover-3-title white text-uppercase font-weight-bold mb-1">JOIN THE BROTHERHOOD</h3>
-                  <Link to='/rush' class= "hover-3-description small text-uppercase mb-0"> RUSH > </Link>
+                  <h3 class="hover-3-title black text-uppercase font-weight-bold mb-1">JOIN THE BROTHERHOOD</h3>
+                  <Link to='/rush' class= "hover-3-description text-uppercase mb-0"> RUSH > </Link>
                 </div>
               ) : (
-                <div class="hover-3-content px-5 py-4">
+                <div class="contact-info-content px-5 py-4">
                   <h3 class="hover-3-title white text-uppercase font-weight-bold mb-1">CONTACT US</h3>
-                  <Link to='/rush' class= "hover-3-description small text-uppercase mb-0"> REGENT & VICE REGENT > </Link>
-                  <Link to='/rush' class= "hover-3-description small text-uppercase mb-0"> EXECUTIVE BOARD > </Link>
-                  <Link to='/rush' class= "hover-3-description small text-uppercase mb-0"> ALUMNI & INTERCHAPTER RELATIONS> </Link>
-                  <Link to='/rush' class= "hover-3-description small text-uppercase mb-0"> COOPORATE & SPONSORSHIP EVENTS> </Link>
+                  <a href="mailto:tht-regents@umich.edu" class= "hover-3-description small text-uppercase mb-0"> REGENT & VICE REGENT > </a>
+                  <a href="mailto:tht-eboard@umich.edu" class= "hover-3-description small text-uppercase mb-0"> EXECUTIVE BOARD > </a>
+                  <a href="mailto:tht-corsec@umich.edu" class= "hover-3-description small text-uppercase mb-0"> ALUMNI & INTERCHAPTER RELATIONS> </a>
+                  <a href="mailto:tht-corporate@umich.edu" class= "hover-3-description small text-uppercase mb-0"> COOPORATE & SPONSORSHIP EVENTS> </a>
                 </div>
               ) }
             </div>
